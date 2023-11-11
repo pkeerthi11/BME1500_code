2370edcca4ca42d73e132ff997a8943811f29b1f
ozenc taskin <HuseyinOzenc.Taskin@mail.utoronto.ca>
2023-11-11 07:23:17 -0500

    Add a demo case for fsaverage copy

--- run_demo.py
+++ run_demo.py
@@ -1,10 +1,11 @@
 # To run this script you need the following packages: mne, autoreject, nibabel,
-# pyvistaqt. All can be installed with pip. 
+# pyvistaqt, finnpy. All can be installed with pip. 
 
 import os, mne
 from preprocessData import preprocessData
 from mne_sourceReconstruction import mne_sourceReconstruction
 from averageSourcesInLabel import averageSourcesInLabel
+from finnpy.source_reconstruction.mri_anatomy import copy_fs_avg_anatomy
 
 # path to the data
 sample_data_folder = mne.datasets.sample.data_path()
@@ -14,10 +15,20 @@ room_noise = (sample_data_folder / 'MEG' / 'sample' / 'ernoise_raw.fif')
 # Subject paths
 subjects_dir = (sample_data_folder / 'subjects')
 
-# Get a copy of the sample data folder for this demo
-sample_folder = os.path.join(subjects_dir, 'sample')
-sample_copy = os.path.join(subjects_dir, 'sample_copy')
-os.system('cp -r %s %s' % (sample_folder, sample_copy))
+# If this is set, copy fsaverage. This is to test the no-T1 MRI case.
+hasT1 = True 
+
+# Get a copy of the sample data folder for this demo so we don't mess with MNE
+# sample data. If the copy folder exists, delete it first
+if os.path.exists(os.path.join(subjects_dir, 'sample_copy')):
+    os.system('rm -r %s' % os.path.join(subjects_dir, 'sample_copy'))
+# Copy fsaverage for noT1 case or copy sample subject for the standard case. 
+if hasT1 == False:
+    copy_fs_avg_anatomy(subjects_dir, 'sample_copy', 'sample_copy')
+else:      
+    sample_folder = os.path.join(subjects_dir, 'sample')
+    sample_copy = os.path.join(subjects_dir, 'sample_copy')
+    os.system('cp -r %s %s' % (sample_folder, sample_copy))
 
 # Subject name
 subject = 'sample_copy'
@@ -42,7 +53,7 @@ preprocessed_data = preprocessData(data, 250, 3, subjects_dir, subject, -1, True
 room_readings = preprocessData(data, 250, 0, 'NA', 'NA', -1, False)
 
 # Run source reconstruction
-(stcs, stcs_psd, fsaverage_stcs, fsaverage_stcs_psd) = mne_sourceReconstruction(preprocessed_data, room_readings, subjects_dir, subject, n_jobs)
+(stcs, stcs_psd, fsaverage_stcs, fsaverage_stcs_psd) = mne_sourceReconstruction(preprocessed_data, room_readings, subjects_dir, subject, n_jobs, hasT1=hasT1)
 
 # Run label averaging on fsaverage space. We use the aparc label
 (label_epochs, times, label_epochs_psd, frequencies) = averageSourcesInLabel(subjects_dir, 'fsaverage', fsaverage_stcs, fsaverage_stcs_psd)
