12c5af618802455e324ee85b0064e2a649736d8b
ozenc taskin <HuseyinOzenc.Taskin@mail.utoronto.ca>
2023-11-13 10:29:44 -0500

    Make fsaverage morphing its own function

---
+++ morph_to_fsaverage.py
@@ -0,0 +1,22 @@
+import mne
+
+def morph_to_fsaverage(stcs, hasT1, subjects_dir, subject):
+    
+    # Calculate morph to fsaverage with the first epoch and then warp all epoches 
+    # with this calculation. If the subject didn't have T1, we use a bit of a
+    # hacky solution by warping fsaverage to fsavarage to have same amount of 
+    # vertices across subjects at the end by subjecting an interpolation. 
+    if hasT1 == True:
+        morph = mne.compute_source_morph(stcs[0], subject_from=subject, subject_to='fsaverage', subjects_dir=subjects_dir)  
+    else:
+        # Here we set source subject to fsaverage and morph fsaverage to itself
+        # just to do the interpolation.
+        for ver in stcs:
+            ver.subject = 'fsaverage'
+        morph = mne.compute_source_morph(stcs[0], subject_from='fsaverage', subject_to='fsaverage', subjects_dir=subjects_dir) 
+    morphed_stcs = []
+    for i in range(len(stcs)):
+        print('Applying fsaverage transformation to epoch number: %s' % i)
+        morphed_stcs.append(morph.apply(stcs[i]))
+        
+    return (morphed_stcs)
