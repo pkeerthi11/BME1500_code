8befae336cf22f5f434d3216fc3e44dc30357993
ozenc taskin <HuseyinOzenc.Taskin@mail.utoronto.ca>
2023-11-16 02:40:32 -0500

    Run freesurfer without removing labels. We'll do connectivity on subject coordinates with labels morphed from fsaverage

--- run_freesurfer.py
+++ run_freesurfer.py
@@ -1,17 +1,25 @@
 # This script runs freesurfer recon-all for all subjects that have T1
-
-from finnpy.source_reconstruction.utils import init_fs_paths   
-from finnpy.source_reconstruction.mri_anatomy import extract_anatomy_from_mri_using_fs
 import os    
 
-# Set FS paths
-init_fs_paths('/home/bme1500trd/freesurfer/subjects')
+# Subject list
+subjects = ['al0067a',
+            'al0064a',
+            'al0053a',
+            'al0051a',
+            'al0049a',
+            'al0047a',
+            'al0043a',
+            'al0042a',
+            'al0040a',
+            'al0039a',
+            'al0029a',
+            'al0011a']
 
 # Loop through subjects, get niftis and run recon-all 
 mri_folder = '/home/bme1500trd/workspace/data/TRD/MRI'
-for i in os.listdir(mri_folder):
+for i in subjects:
     subject_folder = os.path.join(mri_folder, i, 'dcm')
     for sub in os.listdir(subject_folder):
         if sub[-3:] == 'nii':
             mri_image = os.path.join(subject_folder, sub)
-            extract_anatomy_from_mri_using_fs(i, mri_image, overwrite = True)
+            os.system('recon-all -s %s -i %s -all -threads 8' % (i, mri_image))
